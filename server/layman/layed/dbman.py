f7ffd086ee096c349054bece82118467017bb5e1
Michal Šrédl <sredl@ccss.cz>
2014-01-09 16:42:29 +0100

    Fix Create Schema ()

--- server/layman/layed/dbman.py
+++ server/layman/layed/dbman.py
@@ -231,14 +231,13 @@ class DbMan:
             conn = psycopg2.connect(self.getConnectionString())
             cur = conn.cursor()
 
-            # SQL = "SELECT schema_name FROM information_schema.schemata WHERE schema_name = %s;"
-            SQL = "SELECT schema_name FROM information_schema.schemata;"
-            #params = (dbSchema, )
-            #logging.debug("[DbMan][createSchemaIfNotExists] Checking schema '%s'..."% dbSchema)
-            #logging.debug("[DbMan][createSchemaIfNotExists] SQL: '%s', Params: '%s'..."% (SQL, params))
-            logging.debug("[DbMan][createSchemaIfNotExists] SQL: '%s'"% (SQL))
-            #cur.execute(SQL, params)
-            cur.execute(SQL)
+            SQL = "SELECT schema_name FROM information_schema.schemata WHERE schema_name = %s;"
+            # SQL = "SELECT schema_name FROM information_schema.schemata;"
+            params = (dbSchema, )
+            logging.debug("[DbMan][createSchemaIfNotExists] Checking schema '%s'..."% dbSchema)
+            logging.debug("[DbMan][createSchemaIfNotExists] SQL: '%s', Params: '%s'..."% (SQL, params))
+            cur.execute(SQL, params)
+            #cur.execute(SQL)
             result = cur.fetchall()
             logging.debug("[DbMan][createSchemaIfNotExists] Select result: '%s'"% str(result))
 
