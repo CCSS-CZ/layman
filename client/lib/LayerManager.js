699bdd635f530aeb07bbbb402f49ed0b4e37fae4
Michal Šrédl <sredl@ccss.cz>
2014-03-13 18:03:20 +0100

    fix

--- client/lib/LayerManager.js
+++ client/lib/LayerManager.js
@@ -88,8 +88,8 @@ Ext4.define('HSRS.LayerManager', {
         this.layersPanel.store.load();
     },
 
-    _onDataPublished: function(name) {
-        Ext4.Msg.alert('Success', 'Data ['+ name + '] published');
+    _onDataPublished: function(data) {
+        Ext4.Msg.alert('Success', 'Data ['+ data.name + '] published');
         this.layersPanel.store.load();
     },
 
