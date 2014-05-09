696d9bb783d26ccc1cc74fc16833f4d6333873c6
Michal Šrédl <sredl@ccss.cz>
2014-05-09 15:17:58 +0200

    fix

--- client/lib/LayerManager/PublishForm.js
+++ client/lib/LayerManager/PublishForm.js
@@ -248,7 +248,7 @@ Ext4.define('HSRS.LayerManager.PublishForm', {
                                           false : true),
                                anchor: '100%',
                                value: (config.type == "data" ?
-                                       this.group : undefined)
+                                       config.group : undefined)
                            },
                           /* Title field
                            */
