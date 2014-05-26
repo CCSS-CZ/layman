ecbdef7584eb5b7173349360bafde07b798dc8ee
Michal Šrédl <sredl@ccss.cz>
2014-05-26 19:12:26 +0200

    fix

--- client/lib/LayerManager/PublishForm.js
+++ client/lib/LayerManager/PublishForm.js
@@ -392,7 +392,7 @@ Ext4.define('HSRS.LayerManager.PublishForm', {
                                 boxLabel: 'Secure layer',
                                 name: 'secureLayer',
                                 id: 'secureLayer',
-                                checked: secureLayer in config ?
+                                checked: 'secureLayer' in config ?
                                          config.secureLayer : true,
                                 inputValue: true,
                                 uncheckedValue: false,
@@ -409,7 +409,7 @@ Ext4.define('HSRS.LayerManager.PublishForm', {
                                 xtype: 'itemselector',
                                 name: 'read_groups',
                                 id: 'read_groups',
-                                disabled: secureLayer in config ?
+                                disabled: 'secureLayer' in config ?
                                           !config.secureLayer : false,
                                 anchor: '100%',
                                 //width: '100%',
