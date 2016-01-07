3f45627424d7966702b15ff954e64bc23cf099d5
Michal Sredl <sredl@ccss.cz>
2016-01-07 17:02:17 +0100

    License - Client (GPL v3)

--- client/lib/LayerManager/DataPanel/Model.js
+++ client/lib/LayerManager/DataPanel/Model.js
@@ -1,3 +1,23 @@
+/*
+    LayMan - the Layer Manager
+
+    Copyright (C) 2016 Czech Centre for Science and Society
+    Authors: Jachym Cepicky, Karel Charvat, Stepan Kafka, Michal Sredl, Premysl Vohnout
+    Mailto: sredl@ccss.cz, charvat@ccss.cz
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 Ext4.define('HSRS.LayerManager.DataPanel.Model', {
         extend: 'Ext4.data.Model',
