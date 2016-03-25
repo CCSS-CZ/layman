4067f2e799dd352577c5dc81fb8d39f33bb5f60b
Přemysl Vohnout <vohnout@ccss.cz>
2016-03-25 07:40:12 +0100

    Gitignore and list of dependencies

--- server/layman/layed/catman.py
+++ server/layman/layed/catman.py
@@ -25,7 +25,7 @@ import sys
 
 class CatMan:
     """Micka manager
-    Create, get, update and delete metadata.    
+    Create, get, update and delete metadata.
     """
 
     config = None
@@ -48,33 +48,33 @@ class CatMan:
 
     # METADATA OF DATA
 
-    def createDataRecord(self """, params """): # zjistit od stepana, ktery vsechny parametry jsou potreba pro minimalni validni record
-    """ Create new metadata record for data
-    """
+    def createDataRecord(self, params): # zjistit od stepana, ktery vsechny parametry jsou potreba pro minimalni validni record
+        """ Create new metadata record for data
+        """
         return uuid
-       
+
     def getDataRecord(self, dataname, datagroup, datatype):
-    """ Get metadata record for given data
-    Params:
-    dataname - name of view/table/file
-    datagroup - group name (schema or directory)
-    datatype - [table|view|file]""" # mohli bychom zavest nejake "dataid", pak bychom to ale museli vsude zpropagovat vcetne klienta
-    # takhle v tuto chvili vypada jednoznacna reference
-    # uuid bude ulozene v datapadu 
+        """ Get metadata record for given data
+        Params:
+        dataname - name of view/table/file
+        datagroup - group name (schema or directory)
+        datatype - [table|view|file]""" # mohli bychom zavest nejake "dataid", pak bychom to ale museli vsude zpropagovat vcetne klienta
+        # takhle v tuto chvili vypada jednoznacna reference
+        # uuid bude ulozene v datapadu
 
         return result # ziskana metadata v jsonu
 
-    def updateDataRecord((self, dataname, datagroup, datatype """, params to update """): 
-    """ Update metadata record of given data
-    """
+    def updateDataRecord(self, dataname, datagroup, datatype, params):
+        """ Update metadata record of given data
+        """
 
-       return result # jak to dopadlo
+        return result # jak to dopadlo
 
     def deleteDataRecord(self, dataname, datagroup, datatype):
-    """ Delete metadata record of given data 
-    """
+        """ Delete metadata record of given data
+        """
 
-    # METADATA OF SERVICES
+        # METADATA OF SERVICES
 
-    # Budeme potrebovat jeste metadata sluzeb. Cilem je, aby publikovane vrstvy/sluzby byly vyhledatelne v Micce v MapComposeru...
-    
+        # Budeme potrebovat jeste metadata sluzeb. Cilem je, aby publikovane vrstvy/sluzby byly vyhledatelne v Micce v MapComposeru...
+
